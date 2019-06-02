14fdaece2705cb09415c7c98261b7b5e86b6b645
kaustubh kadam <kadamkaustubh5753@gmail.com>
2019-06-02 01:18:34 -0400

    working numbers game

--- numbers-game.js
+++ numbers-game.js
@@ -1,34 +1,74 @@
+var problem, answers
 
-var problem = createNumbersAndTarget(2)
-var width = 50
-console.log(problem)
-console.log(solver(problem))
-var bar = document.getElementById('p3')
-var i = 0
-var interval = setInterval(function(){
-    console.log(i)
-    bar.MaterialProgress.setProgress(i*3)
-    i++
-    if(i>20){
-        clearInterval(interval)
+var slide = document.getElementById('big-number-selector'),
+    sliderDiv = document.getElementById("big-number-out");
+
+var submit_button = document.getElementById('number-submit')
+
+submit_button.onclick = displayNumbers
+async function displayNumbers() {
+    console.log('pressed')
+    submit_button.disabled = true
+    problem = createNumbersAndTarget(slide.value)
+    console.log(problem.numbers)
+    var showNumbers = document.getElementById('show-numbers')
+    for (var i = 0; i < problem.numbers.length; i++) {
+        var num = document.createTextNode(problem.numbers[i] + '  ')
+        showNumbers.appendChild(num)
+        componentHandler.upgradeElement(showNumbers)
+        await sleep(800)
     }
-},1000)
+    var show_target = document.getElementById('show-target')
+    var target = document.createTextNode(problem.target)
+    show_target.appendChild(target)
+    startTheClock()
+    answers = solver(problem)
+}
 
-function setupCanvas(canvas) {
-    // Get the device pixel ratio, falling back to 1.
-    var dpr = window.devicePixelRatio || 1;
-    // Get the size of the canvas in CSS pixels.
-    var rect = canvas.getBoundingClientRect();
-    // Give the canvas pixel dimensions of their CSS
-    // size * the device pixel ratio.
-    canvas.width = rect.width * dpr;
-    canvas.height = rect.height * dpr;
-    var ctx = canvas.getContext('2d');
-    // Scale all drawing operations by the dpr, so you
-    // don't have to worry about the difference.
-    ctx.scale(dpr, dpr);
-    return ctx;
-  }
 
-var draw = SVG('canvas').size(300,300)
-var rect = draw.rect(100, 100)
+
+function startTheClock() {
+    var bar = document.getElementById('clock')
+    var i = 0
+    var interval = setInterval(function () {
+        bar.MaterialProgress.setProgress(i / 10)
+        i++
+        if (i > 1000) {
+            clearInterval(interval)
+            console.log(answers)
+            displayAnswers()
+        }
+    }, 30)
+}
+function displayAnswers(){
+    var cell = document.getElementById('answers-out')
+    for(var i=0; i<answers.length;i++){
+        var text = document.createTextNode(answers[i]+'\n')
+        var br = document.createElement('br')
+        cell.appendChild(text)
+        cell.appendChild(br)
+    }
+}
+function createCardTitle(cell, title) {
+    var cardTitle = document.createElement('div')
+    var titleElement = document.createElement('h2')
+    var titleText = document.createTextNode(title)
+    titleElement.className = "mdl-card__title-text"
+    titleElement.id = title
+    titleElement.appendChild(titleText)
+    componentHandler.upgradeElement(titleElement)
+    cardTitle.className = 'mdl-card__title'
+    cardTitle.appendChild(titleElement)
+    componentHandler.upgradeElement(cardTitle)
+    cell.appendChild(cardTitle)
+    return cell
+}
+
+slide.onchange = function () {
+    var suffix = this.value == 1 ? ' big number' : ' big numbers'
+    sliderDiv.innerHTML = this.value + suffix;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
