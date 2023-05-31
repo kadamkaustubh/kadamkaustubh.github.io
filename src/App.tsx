5d84d5f1a8089cfe8eb0c80d0bd1f03f0789f04b
kaustubh kadam <kadamkaustubh5753@gmail.com>
2023-05-31 02:49:31 -0400

    reformat

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title={"Hello World"}/>
+    </div>
+  );
+}
+
+export default App;
